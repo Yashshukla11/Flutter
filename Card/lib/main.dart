4d5f2dd285caeaf670795c2a7f5629c6aacdf0aa
Yash Shukla <112846526+Yashshukla11@users.noreply.github.com>
2024-01-31 12:01:24 +0530

    used icon , card , Listtile in place of image , container , row/col

--- Card/lib/main.dart
+++ Card/lib/main.dart
@@ -10,120 +10,76 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            // Wrap the Column with a Center widget
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment
-                  .center, // Align children horizontally center
-              children: [
-                const CircleAvatar(
-                  // backgroundColor: Colors.pink,
-                  backgroundImage: AssetImage('images/yash.png'),
-                  maxRadius: 70,
-                  minRadius: 30,
-                ),
-
-                const Text(
-                  'Yash Shukla',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Ananda',
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.bold,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  const CircleAvatar(
+                    // backgroundColor: Colors.pink,
+                    backgroundImage: AssetImage('images/yash.png'),
+                    maxRadius: 70,
+                    minRadius: 30,
                   ),
-                ),
-                const Text(
-                  "FLUTTER DEVELOPER",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Wible Town Standard Demo',
-                    fontSize: 20,
-
+                  const Text(
+                    'Yash Shukla',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Ananda',
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Container(
-                  height: 50.0,
-                  width: 300.0,
-                  padding: const EdgeInsets.only(left: 10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                        12.0), // Adjust the value to change the roundness of the border
-                    color: Colors.white,
-                  ),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image(
+                  const Text(
+                    "FLUTTER DEVELOPER",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Wible Town Standard Demo',
+                      fontSize: 20,
 
-                        image: NetworkImage(
-                            'https://i.postimg.cc/wTfwyYZd/phone-call.png'),
-                        // fit: BoxFit.contain,
-                        width: 30,
-                        height: 30,
+                    ),
+                  ),
+                   SizedBox(
+                    height: 20,
+                    width: 150.0,
+                    child: Divider(
+                      color: Colors.teal.shade200,
+                    ),
+                  ),//for horizontal line
+                  Card(
+                    color: Colors.white,
+                    margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: const Icon(
+                        Icons.phone,
                         color: Colors.teal,
                       ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        '+91 9555144734',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Wible Town Standard Demo',
-                          fontSize: 15,
-                        ),
-                      )
-                    ],
+                      title:
+                      Text('+91 9555144734',
+                      style: TextStyle(fontSize: 15, color: Colors.teal.shade900 ,fontFamily: 'Wible Town Standard Demo'),
+                    ),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 50.0,
-                  width: 300.0,
-                  padding: const EdgeInsets.only(left: 10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                        12.0), // Adjust the value to change the roundness of the border
+                  Card(
                     color: Colors.white,
-                  ),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image(
-
-                        image: NetworkImage(
-                            'https://i.postimg.cc/6QRm7Pt9/email.png'),
-                        // fit: BoxFit.contain,
-                        width: 30,
-                        height: 30,
+                    margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: const Icon(
+                        Icons.mail,
                         color: Colors.teal,
                       ),
-                      SizedBox(
-                        width: 20,
+                      title:
+                      Text('yashshukla2003@gmail.com',
+                        style: TextStyle(fontSize: 15, color: Colors.teal.shade900 ,fontFamily: 'Wible Town Standard Demo'),
                       ),
-                      Text(
-                        'yashshukla2003@gmail.com',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Wible Town Standard Demo',
-                          fontSize: 15,
-                        ),
-                      )
-                    ],
+                    ),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
-          ),
         ),
       ),
     );
